73463b6b4c2c12138dfa8e1de12813a91307ade0
duxo-dev <duxos101@gmail.com>
2025-10-27 03:48:34 +0000

    Update user.js

--- api/user.js
+++ api/user.js
@@ -1,4 +1,11 @@
 export default async function handler(req, res) {
-  res.status(200).json({ message: "Hello from backend!" });
-}
+  const { token } = req.query;
+  if (!token) return res.status(400).json({ error: "No token provided" });
 
+  const userResponse = await fetch("https://discord.com/api/users/@me", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  const userData = await userResponse.json();
+  return res.status(200).json(userData);
+}
