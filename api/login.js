d1bc1cc363c32389e870c098be4e96f68dfbe192
duxo-dev <duxos101@gmail.com>
2025-10-27 03:55:28 +0000

    Refactor Discord OAuth URL generation

--- api/login.js
+++ api/login.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
-  const redirectUri = encodeURIComponent("https://duxo-dev-backend.vercel.app/api/callback");
-  const clientId = "YOUR_DISCORD_CLIENT_ID"; // replace with yours
-
-  const discordAuthUrl = `https://discord.com/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=identify`;
-
-  return res.redirect(discordAuthUrl);
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const REDIRECT_URI = process.env.REDIRECT_URI;
+  const scope = encodeURIComponent("identify");
+  const redirect = `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code&scope=${scope}`;
+  return res.redirect(redirect);
 }
